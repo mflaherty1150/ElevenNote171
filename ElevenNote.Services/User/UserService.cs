f6489eb470f3113dbb73666949ce2a751d9f7add
Martin Flaherty <mflaherty@elevenfifty.org>
2023-08-01 17:05:10 -0400

    User Register build out

--- ElevenNote.Services/User/UserService.cs
+++ ElevenNote.Services/User/UserService.cs
@@ -1,6 +1,57 @@
+using ElevenNote.Data;
+using ElevenNote.Data.Entities;
+using ElevenNote.Models.User;
+using Microsoft.AspNetCore.Identity;
+
 namespace ElevenNote.Services.User;
 
 public class UserService : IUserService
 {
-    
+    private readonly ApplicationDbContext _context;
+    private readonly UserManager<UserEntity> _userManager;
+    private readonly SignInManager<UserEntity> _signInManager;
+
+    public UserService(ApplicationDbContext context, UserManager<UserEntity> userManager, SignInManager<UserEntity> signInManager)
+    {
+        _context = context;
+        _userManager = userManager;
+        _signInManager = signInManager;
+    }
+    public async Task<bool> RegisterUserAsync(UserRegister model)
+    {
+        if (await CheckEmailAvailabilityAsync(model.Email) == false)
+        {
+            System.Console.WriteLine("Invalid email, already in use.");
+            return false;
+        }
+
+        if (await CheckUserNameAvailabilityAsync(model.UserName) == false)
+        {
+            System.Console.WriteLine("Invalid username, already in use.");
+            return false;
+        }
+        
+        UserEntity entity = new()
+        {
+            Email = model.Email,
+            UserName = model.UserName,
+            DateCreated = DateTime.Now
+        };
+
+        IdentityResult registerResult = await _userManager.CreateAsync(entity, model.Password);
+
+        return registerResult.Succeeded;
+    }
+
+    private async Task<bool> CheckUserNameAvailabilityAsync(string userName)
+    {
+        UserEntity? existingUser = await _userManager.FindByNameAsync(userName);
+        return existingUser is null;
+    }
+
+    private async Task<bool> CheckEmailAvailabilityAsync(string email)
+    {
+        UserEntity? existingUser = await _userManager.FindByEmailAsync(email);
+        return existingUser is null;
+    }
 }
