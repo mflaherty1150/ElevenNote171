d7becaf5dfe0db3f0284cc6feb37f05503e180bd
Martin Flaherty <mflaherty@elevenfifty.org>
2023-08-02 11:58:30 -0400

    Added GetUserByIdAsync functionality

--- ElevenNote.Services/User/UserService.cs
+++ ElevenNote.Services/User/UserService.cs
@@ -30,7 +30,7 @@ public class UserService : IUserService
             System.Console.WriteLine("Invalid username, already in use.");
             return false;
         }
-        
+
         UserEntity entity = new()
         {
             Email = model.Email,
@@ -41,6 +41,24 @@ public class UserService : IUserService
         IdentityResult registerResult = await _userManager.CreateAsync(entity, model.Password);
 
         return registerResult.Succeeded;
+    }
+
+    public async Task<UserDetail?> GetUserByIdAsync(int userId)
+    {
+        UserEntity? entity = await _context.Users.FindAsync(userId);
+        if (entity is null) return null;
+
+        UserDetail detail = new()
+        {
+            Id = entity.Id,
+            Email = entity.Email,
+            UserName = entity.UserName,
+            FirstName = entity.FirstName,
+            LastName = entity.LastName,
+            DateCreated = entity.DateCreated
+        };
+
+        return detail;
     }
 
     private async Task<bool> CheckUserNameAvailabilityAsync(string userName)
